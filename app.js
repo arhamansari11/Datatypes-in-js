a021b2bcdc662da8bae5fb81178ea81d6204b9ba
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2023-08-11 09:42:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let a = 0;
+let number1 = 346;
+let string1 = "Arham";
+let Bollean = true;
+let bigint1 = BigInt("43") - BigInt("3")
+let symbol1 = Symbol("I m a symbol")
+var nullVar = null;
+
+console.log(nullVar)
+console.log(typeof a)
+console.log(typeof number1)
+console.log(typeof string1)
+console.log(typeof Bollean)
+console.log(typeof bigint1)
+console.log(typeof symbol1)
+
+
+let myobj = {
+    name1 : "Arham",
+    marks : 346,
+    Boolean : true,
+}
+console.log(`i am `,myobj["name1"]);
+console.log(typeof myobj)
